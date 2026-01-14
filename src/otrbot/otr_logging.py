618622d1136acf9d2e90eaa1e5d6e4d63d9736fc
Gáll Zoltán <gallzoltan.gmail.com>
2026-01-14 13:53:36 +0100

    fixed logging to encoding utf8

--- src/otrbot/otr_logging.py
+++ src/otrbot/otr_logging.py
@@ -1,5 +1,12 @@
 import logging
-from logging import config
+import logging.config
+import sys
+
+# Reconfigure stdout to use UTF-8 encoding for Hungarian characters
+if sys.stdout.encoding != 'utf-8':
+    sys.stdout.reconfigure(encoding='utf-8', errors='replace')
+if sys.stderr.encoding != 'utf-8':
+    sys.stderr.reconfigure(encoding='utf-8', errors='replace')
 
 log_config = {
   "version":1,
@@ -12,14 +19,15 @@ log_config = {
       "formatter": "std_out",
       "class": "logging.StreamHandler",
       "level": "DEBUG"
-    },        
+    },
     "file":{
       "formatter": "std_out",
       "class": "logging.FileHandler",
       "filename": "./logs/otr.log",
       "mode":"w",
-      "level": "DEBUG"
-    }        
+      "level": "DEBUG",
+      "encoding": "utf-8"
+    }
   },
   "formatters":{
     "std_out": {
@@ -29,13 +37,5 @@ log_config = {
   },
 }
 
-config.dictConfig(log_config)
+logging.config.dictConfig(log_config)
 logger = logging.getLogger('OTR')
-
-# _formatter = logging.Formatter("%(asctime)s | %(levelname)s | %(name)s | %(message)s")
-# _handlers = [logging.StreamHandler(), logging.FileHandler('./logs/file.log', mode='w')]
-# logger = logging.getLogger('OTR')
-# for handler in _handlers:
-#   handler.setFormatter(_formatter)
-#   logger.addHandler(handler)  
-# logger.setLevel(logging.DEBUG)
